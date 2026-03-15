987b3d961985988208298d0c7de5e262927324d3
WinningCloud <farhan84897@gmail.com>
2026-03-15 16:46:16 +0530

    Added render backend url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://namma-campus-backend.onrender.com/api',
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 });
